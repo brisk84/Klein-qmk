547bc77e8c2d384db86720573c549018bb768bff
marat <gumyarov@bzaem.com>
2023-04-16 16:32:15 +0300

    feat(layout): add colamak, encoders

--- config.h
+++ config.h
@@ -30,7 +30,7 @@
 #ifdef POINTING_DEVICE_ENABLE
     #define POINTING_DEVICE_ROTATION_90
     #define CIRQUE_PINNACLE_DIAMETER_MM 35
-    
+
     #define CIRQUE_PINNACLE_TAP_ENABLE
     // #define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
     // #define POINTING_DEVICE_GESTURES_CURSOR_GLIDE_ENABLE
@@ -45,3 +45,23 @@
 #ifdef AUDIO_ENABLE
     #define NO_MUSIC_MODE
 #endif
+
+#define TAPPING_TERM 170
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// ┌─────────────────────────────────────────────────┐
+// │ e n c o d e r s                                 │
+// └─────────────────────────────────────────────────┘
+
+// #define ENCODERS_PAD_A { F4 } //PIN A3
+// #define ENCODERS_PAD_B { F5 } //PIN A2
+#define ENCODERS_PAD_A       { D1 }
+#define ENCODERS_PAD_B       { D0 }
+#define ENCODERS_PAD_A_RIGHT { D0 }
+#define ENCODERS_PAD_B_RIGHT { D1 }
+#define ENCODER_RESOLUTION 2
