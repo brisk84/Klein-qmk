badf2b6b7e392004eef11882e00bbd3d92f4b948
marat <gumyarov@bzaem.com>
2023-04-17 00:07:57 +0300

    fix(layout): swap brackets

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -66,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [L_NUM] = LAYOUT_split_3x5_4(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        KC_LBRC, KC_GRV, KC_LCBR, KC_RCBR,  KC_RBRC,      KC_NO,   KC_7, KC_8, KC_9,   KC_0,
+        KC_LBRC, KC_GRV, KC_LPRN, KC_RPRN,  KC_RBRC,      KC_NO,   KC_7, KC_8, KC_9,   KC_0,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
         KC_NO,   KC_EXLM, KC_COLN, KC_EQL, KC_PLUS,  KC_NO,   KC_4, KC_5, KC_6,   KC_DOT,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-        KC_UNDS,  KC_LPRN, KC_SCLN, KC_RPRN, KC_MINS,      KC_NO,   KC_1, KC_2, KC_3,   KC_BSLS,
+        KC_UNDS,  KC_LCBR, KC_SCLN, KC_RCBR, KC_MINS,      KC_NO,   KC_1, KC_2, KC_3,   KC_BSLS,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
                    KC_DOT, KC_0, KC_MINS, QK_BOOT,    QK_BOOT, KC_ENT, KC_BSPC, KC_DEL
   //              ╰────────────────────────────────╯ ╰────────────────────────────────╯
